c1d174469a7e4482965529a2d10a5e9d3da9677e
Márcio Levi Sales Prado <marciolevisalesprado@gmail.com>
2022-02-21 16:47:01 -0300

    Trocando de jogador a cada turno

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -12,13 +12,13 @@ public abstract class ChessPiece extends Piece {
 		super(board);
 		this.color = color;
 	}
-	
+
 	public Color getColor() {
 		return color;
 	}
-	
+
 	protected boolean isThereOpponentPiece(Position position) {
-		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
 		return p != null && p.getColor() != color;
 	}
 }
